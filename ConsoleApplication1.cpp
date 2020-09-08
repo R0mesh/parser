339304b206fd47f15d8fdb9560b84a3b431a5dbb
Роман <romesh@inbox.ru>
2020-09-08 09:36:24 +0400

    add

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>;
+﻿#pragma comment(lib,"curllib.lib")
+#include <iostream>;
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -11,7 +12,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Starting parsing...\n";
-    cout << "Complite parsing\n";
+    CURL* curl_handle = curl_easy_init();
+    char link[128] = "https://api.stackexchange.com/docs/search#order=desc&sort=activity&intitle=beautiful&filter=default&site=stackoverflow&run=true";
+    cout << "Enter link address" << endl;
+    //cin >> link;
+    cout << "Starting parsing..." << endl;
+    if (curl_handle)
+    {
+        curl_easy_setopt(curl_handle, CURLOPT_URL, link);
+        CURLcode res = curl_easy_perform(curl_handle);
+        curl_easy_cleanup(curl_handle);
+    }
+    cout << "Complite parsing" << endl;
 }
 
